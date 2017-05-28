c1fd684a7f8e01b1220c7adc72e60d296d7b38c3
Bass Jansson <bassjansson@gmail.com>
2017-05-28 20:22:05 +0200

    Adds an idea for an AM/FM demodulator.

--- research/dsp/convolution/Main.cpp
+++ research/dsp/convolution/Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, const char* argv[])
     string mode = "spectrum";
 
     sigi sampleRate = 48000;
-    sigi N = 4096;
+    sigi N = 2048;
 
 
     if (argc > 1)
@@ -26,37 +26,83 @@ int main(int argc, const char* argv[])
 
 
     sigc* input    = new sigc[N];
+    sigc* mask     = new sigc[N];
     sigc* output   = new sigc[N];
     sigc* spectrum = new sigc[N];
 
 
-    sigf freq = 300; // Hz
-    sigi rhythm = N / 1.4;
+    sigf modFreq = 100; // Hz
 
-    for (sigi n = 0; n < N / 2; ++n)
+    sigf carFreqOffset = 300;
+    sigf carFreqDepth = 150;
+    sigf carPhase = 0;
+
+    for (sigi n = 0; n < N; ++n)
     {
-        input[n] = SIG_TWO * sin(SIG_TWO * M_PI * freq / sampleRate * n) * (n % rhythm) / rhythm;
+        float carFreq = carFreqOffset + carFreqDepth * cos(SIG_TWO * M_PI * modFreq / sampleRate * n);
+        carPhase += SIG_TWO * M_PI * carFreq / sampleRate;
+
+        float AM = sin(SIG_TWO * M_PI * modFreq / sampleRate * n) * 0.48f + 0.52f;
+
+        input[n] = sigc(cos(carPhase), sin(carPhase)) * AM;
         //input[n] = (float)rand() / RAND_MAX * 2.0 - 1.0;
     }
 
-    rhythm = N / 4.5;
-
-    for (sigi n = N / 2; n < N; ++n)
+    for (sigi n = 0; n < N; ++n)
     {
-        input[n] = SIG_TWO * sin(SIG_TWO * M_PI * freq / sampleRate * n) * (n % rhythm) / rhythm;
-        //input[n] = (float)rand() / RAND_MAX * 2.0 - 1.0;
+        // TODO: it still goes wrong here, creating an imaginary part is hard
+        //input[n] = sigc(real(input[n]), real(input[n] - input[(n + 1) % N]) * exp(M_PI));
     }
 
 
-    FourierTransform ft(N);
-
-    if (mode == "input")
+    if (mode == "inputRe")
     {
         for (sigi n = 0; n < N; ++n)
             cout << n << "\t" << real(input[n]) << "\n";
     }
 
+    if (mode == "inputIm")
+    {
+        for (sigi n = 0; n < N; ++n)
+            cout << n << "\t" << imag(input[n]) << "\n";
+    }
+
+    // AM/FM Demodulator
+    for (sigi n = 0; n < N; ++n)
+    {
+        float ampExp = exp(real(log(input[n])));
+        float freqDiff = imag(log(input[(n + 1) % N] / input[n]));
+
+        if (freqDiff < 0)
+            freqDiff += SIG_TWO * M_PI;
+
+        output[n] = sigc(ampExp, freqDiff);
+    }
+
+
+    if (mode == "ampMod")
+    {
+        for (sigi n = 0; n < N; ++n)
+            cout << n << "\t" << real(output[n]) << "\n";
+    }
+
+    if (mode == "freqMod")
+    {
+        for (sigi n = 0; n < N; ++n)
+        {
+            cout << n << "\t" << imag(output[n]) / (SIG_TWO * M_PI) * sampleRate / carFreqOffset;
+            cout << "\n";
+        }
+    }
+
+    /*
+    FourierTransform ft(N);
+
     ft.forward(input, spectrum);
+    ft.forward(mask, output);
+
+    for (sigi k = 0; k < N; ++k)
+        spectrum[k] /= output[k] * (sigf)N;
 
     if (mode == "response")
     {
@@ -80,6 +126,8 @@ int main(int argc, const char* argv[])
 
     ft.backward(spectrum, spectrum);
     ft.exponential(spectrum);
+
+
     ft.backward(spectrum, output);
 
     if (mode == "output")
@@ -87,11 +135,12 @@ int main(int argc, const char* argv[])
         for (sigi n = 0; n < N; ++n)
             cout << n << "\t" << real(output[n]) << "\n";
     }
-
+    */
 
 
 
     delete[] input;
+    delete[] mask;
     delete[] output;
     delete[] spectrum;
 
