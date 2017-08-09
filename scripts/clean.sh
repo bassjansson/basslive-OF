1d984521cc436ec88d57bb604735966b5a4a0c91
Bass Jansson <bassjansson@gmail.com>
2017-08-09 17:37:32 +0200

    TG-20 Updates Xcode project and moves it to a separate 'osx' directory.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,8 @@
 # Go to the directory of this script
 cd "${0%/*}"
 
-# Remove build/bin and docs/api directory
-rm -rf ../build/bin
+# Remove the compiled builds
+rm -rf ../build/osx/Builds
+
+# Remove the generated api
 rm -rf ../docs/api
